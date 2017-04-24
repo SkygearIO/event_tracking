da750511dd5a9c49922178af10941c2ab3c8aea9
Louis Chan <louischan@oursky.com>
2017-04-24 19:23:44 +0800

    Rename misleading variable name

--- skygear_event_tracking/tests/utils_test.py
+++ skygear_event_tracking/tests/utils_test.py
@@ -16,14 +16,13 @@ class UtilsTest(unittest.TestCase):
 
     def test_parse_rfc3339(self):
         input_ = '2017-01-23T01:23:45.006789Z'
-        expected = parse_rfc3339(input_)
-        self.assertEqual(2017, expected.year)
-        self.assertEqual(1, expected.month)
-        self.assertEqual(23, expected.day)
-        self.assertEqual(1, expected.hour)
-        self.assertEqual(23, expected.minute)
-        self.assertEqual(45, expected.second)
-        self.assertEqual(6789, expected.microsecond)
+        actual = parse_rfc3339(input_)
+        self.assertEqual(2017, actual.year)
+        self.assertEqual(1, actual.month)
+        self.assertEqual(23, actual.day)
+        self.assertEqual(1, actual.hour)
+        self.assertEqual(23, actual.minute)
+        self.assertEqual(45, actual.second)
+        self.assertEqual(6789, actual.microsecond)
+        self.assertEqual(None, actual.tzinfo)
 
-        # it should be naive datetime
-        self.assertEqual(None, expected.tzinfo)
